34541c9ac4baa7d82a70d25a474d28c9e5fcec54
AustinRado <radoaussy@gmail.com>
2023-10-24 22:14:12 +0300

    header features

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import {useEffect, useRef} from 'react';
+import medicare from '../../assets/images/medicare.jpg';
+import { NavLink, Link } from 'react-router-dom';
+ 
+//array navLinks that redirects to the pages
+const navLinks = [
+  {
+    path:'/home',
+    display:'Home'
+  },
+  {
+    path:'/doctors',
+    display:'Find a Doctor'
+  },
+  {
+    path:'/services',
+    display:'Services'
+  },
+  {
+    path:'/contact',
+    display:'Contact'
+  },
+];
 
 const Header = () => {
   return (
-    <div>
-     Header 
-    </div>
+    <header className='header flex items-center'>
+      <div className='container'>
+        <div className='flex items-center justify-between'>
+          {/*========logo=========*/}
+            <div>
+              <img src={medicare} alt=''/>
+            </div>
+
+            {/*========menu=========*/}
+            <div className='navigation'>
+              <ul className='menu flex items-center gap-[2.7rem]'>
+               {navLinks.map((link, index) => (
+                  <li key = {index}>
+                    <NavLink
+                    to={link.path}
+                    className={navClass => navClass.isActive ? 'text-primaryColor text-[16px] leading-7 font-[600]' :
+                    'text-textColor text-[16px] leading-7 font-[500] '}
+                    >{link.display}
+                    </NavLink>
+                  </li>
+               ))};
+              </ul>
+            </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
