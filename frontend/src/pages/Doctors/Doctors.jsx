73c980df3c7d4520acb4a7bb1f40564b9ed8aab6
AustinRado <radoaussy@gmail.com>
2023-11-03 15:05:22 +0300

    finding a doctor page

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import Doctor from '../../components/Doctor/Doctor';
+import Testimonials from '../../components/Testimonials/Testimonials';
 
 const Doctors = () => {
   return (
-    <div>
-    Doctors
-    </div>
+    <>
+      <section className='bg-[#fff9ea]'>
+        <div className='container text-center'>
+          <h2 className='heading'>Find a doctor</h2>
+          <div className='max-w-[570px] mt-7 mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between'>
+            <input 
+              type='search' 
+              className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none'
+               placeholder='Search for a doctor' />
+               <button className='btn text-black mt-0 rounded-[0px] rounded-r-md'>Search</button>
+          </div>
+        </div>
+        
+      </section>
+      <Doctor/>
+      <Testimonials/>
+    </>
   )
 }
 
