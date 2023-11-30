1fbc18346620ae6ebde6a48d78208010911a5d56
AustinRado <radoaussy@gmail.com>
2023-12-01 00:28:07 +0300

    backend structure

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,33 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
   });
+
+//Database connection
+mongoose.set("strictQuery", false);
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+    console.log('connected to mongoDB');
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//middleware
+app.use(express.json()); // parse incoming JSON requests
+app.use(cookieParser()); //pasrse cookies attached to the clients request (use auth or session identifiers)
+app.use(cors()); // allow cross-origin requests
   
   app.listen(port, () => {
+    connectDB();
     console.log(`Example app listening on port ${port}`);
   });
