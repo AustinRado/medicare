c3179c126565c645e98cf5beae798399aee85fdd
AustinRado <radoaussy@gmail.com>
2024-02-26 15:26:22 +0300

    implementing context API

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,59 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {toast} from 'react-toastify';
+import { HashLoader } from'react-spinners';
+import { BASE_URL } from '../config';
+import {authContext} from '../context/AuthContext.jsx';
 
 const Login = () => {
    
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState({ 
     email:'',
     password:'',
   });
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const {dispatch} = useContext(authContext);
 
   const handleInputChange = e => {
     setFormData({...formData, [e.target.name]: e.target.value});
+  };
+
+  const submitHandler = async(event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      const res = await fetch(`${BASE_URL}/auth/login`,{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      const result = await res.json();
+
+      if(!res.ok){
+        throw new Error(result.message);
+      }
+
+      dispatch({
+        type:'LOGIN_SUCCESS',
+        payload:{
+          user:result.data,
+          token:result.token,
+          role:result.role
+        },
+      });
+      console.log(result, 'login data');
+
+      setIsLoading(false);
+      toast.success(result.message);
+      navigate('/home');
+
+    } catch (error) {
+      toast.error(error.message);
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -19,7 +63,9 @@ const Login = () => {
         Hello <span className='text-primaryColor'>Welcome</span> Back
       </h3> 
 
-       <form className='py-4 md:py-0'>
+       <form 
+        onSubmit={submitHandler}
+        className='py-4 md:py-0'>
         <div className='mb-5 px-4 md:px-2'>
          <input
           className='w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer'
