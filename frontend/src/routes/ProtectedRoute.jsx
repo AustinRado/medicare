f6d4af81261597055bcc3c4969beb6b5db0c9e7d
AustinRado <radoaussy@gmail.com>
2024-02-26 23:02:31 +0300

    implementing protected routes

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -3,9 +3,12 @@ import {Navigate} from 'react-router-dom'
 import { authContext } from '../context/AuthContext'
 
 const ProtectedRoute = ({children, allowedRoles}) => {
-  return (
-    <div>ProtectedRoute</div>
-  )
+
+    const {token, role} = useContext(authContext);
+    const isAllowed = allowedRoles.includes(role);
+    const accessibleRoute = token && isAllowed ? children : <Navigate to="/login" replace={true}/>;
+  
+    return accessibleRoute;
 }
 
 export default ProtectedRoute
