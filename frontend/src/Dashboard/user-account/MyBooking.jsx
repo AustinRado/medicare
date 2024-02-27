39d6b8913f02677d0e0b91d0116fd59ca4f468b9
AustinRado <radoaussy@gmail.com>
2024-02-27 13:19:01 +0300

    added conditional rendering to MyBokking Component

--- frontend/src/Dashboard/user-account/MyBooking.jsx
+++ frontend/src/Dashboard/user-account/MyBooking.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import useFetchData from '../../hooks/useFetchData';
+import { BASE_URL } from '../../config';
+import Loading from '../../components/loader/Loading';
+import Error from '../../components/Error/Error';
+import DoctorCard from '../../components/Doctor/DoctorCard';
 
 const MyBooking = () => {
+
+  const {
+    data:appointments, 
+    loading, 
+    error
+  } = useFetchData(`${BASE_URL}/users/appointments/my-appointment`);
+
   return (
-    <div>MyBooking</div>
+    <div>
+    {loading && !error && <Loading/>}
+    {error && !loading && <Error errMessage={error}/>}
+    {!loading && !error && (
+      <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+        {
+          appointments.map(doctor => (
+            <DoctorCard key={doctor._id} doctor={doctor}/>
+        ))}
+      </div>
+      )}
+      {!loading && !error && appointments.length === 0 && (
+        <h2 className='mt-5 text-center text-primaryColor text-[20px] leading-7 font-semibold'>
+        No Appointments!
+        </h2>
+      )}
+    </div>
   )
 }
 
